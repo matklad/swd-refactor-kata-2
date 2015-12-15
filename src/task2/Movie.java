e05c6e27c21d11b3b9f8d6a6918dfe65c1afeeb4
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-12-15 10:14:57 +0300

    task2

--- src/task2/Movie.java
+++ src/task2/Movie.java
@@ -1,28 +1,13 @@
 package task2;
 
 public class Movie {
-  public static final int CHILDRENS = 2;
-  public static final int REGULAR = 0;
-  public static final int NEW_RELEASE = 1;
+  private final String title;
 
-  private String title;
-  private int priceCode;
-
-  public Movie(String title, int priceCode) {
+  public Movie(String title) {
     this.title = title;
-    this.priceCode = priceCode;
-  }
-
-  public int getPriceCode() {
-    return priceCode;
-  }
-
-  public void setPriceCode(int code) {
-    priceCode = code;
   }
 
   public String getTitle() {
     return title;
   }
-
 }
--- src/task2/Rental.java
+++ src/task2/Rental.java
@@ -1,9 +1,16 @@
 package task2;
 
 public class Rental {
-  public Rental(Movie movie, int daysRented) {
+  enum Type {
+    REGULAR,
+    NEW_RELEASE,
+    CHILDRENS;
+  }
+
+  public Rental(Movie movie, int daysRented, Type type) {
     this.movie = movie;
     this.daysRented = daysRented;
+    this.type = type;
   }
 
   public int getDaysRented() {
@@ -14,6 +21,42 @@ public class Rental {
     return movie;
   }
 
-  private Movie movie;
-  private int daysRented;
+  private final Movie movie;
+  private final int daysRented;
+  private final Type type;
+
+  public boolean isFrequentRenter() {
+    return type == Type.NEW_RELEASE && getDaysRented() > 1;
+  }
+
+  public double calculate() {
+    switch (type) {
+      case REGULAR:
+        return calculateRegularRental();
+      case NEW_RELEASE:
+        return calculateNewReleaseRental();
+      case CHILDRENS:
+        return calculateChildrenRental();
+
+    }
+    throw new IllegalStateException();
+  }
+
+  private double calculateChildrenRental() {
+    double total = 1.5;
+    if (getDaysRented() > 3)
+      total += (getDaysRented() - 3) * 1.5;
+    return total;
+  }
+
+  private double calculateNewReleaseRental() {
+    return getDaysRented() * 3;
+  }
+
+  private double calculateRegularRental() {
+    double total = 2;
+    if (getDaysRented() > 2)
+      total += (getDaysRented() - 2) * 1.5;
+    return total;
+  }
 }
